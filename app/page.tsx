2b37e8b22eb0e6165347ecf5580cf266d8eff902
znareak <libardojesusrengifo129@gmail.com>
2024-09-22 17:03:24 +0200

    ui: user logged navbar

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Index() {
 
   return (
     <>
-      <div className="absolute top-0 z-[-2] w-full h-auto bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div className="absolute top-0 z-[-2] w-full h-dvh bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
 
       <div className="mx-auto flex flex-col items-center justify-center w-full max-w-5xl">
         <Image
--- components/Navbar/userButtonNavbar.tsx
+++ components/Navbar/userButtonNavbar.tsx
@@ -1,9 +1,16 @@
 import { FiLogOut, FiChevronDown, FiUser } from "react-icons/fi";
 import useAuth from "@/hooks/useAuth";
-import NextLink from "next/link";
 import { createClient } from "@/utils/supabase/client";
 import { useState, useEffect } from "react";
 import { SimpleUser } from "@/types";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+  Avatar,
+} from "@nextui-org/react";
 
 export const UserButtonNavbar = () => {
   const supabase = createClient();
@@ -27,63 +34,27 @@ export const UserButtonNavbar = () => {
   }, [user, supabase]);
 
   return (
-    <div className="flex justify-between p-5" id="header-nav-container">
-      <div></div>
-      <div className="flex justify-end gap-4">
-        <div className="relative cursor-pointer">
-          <div
-            onClick={() => document.getElementById("user-menu")?.classList.toggle("open")}
-            className="p-2 rounded-lg transition-all flex items-center"
-          >
-            <div className="flex items-center">
-              <img
-                src={userBasicData?.avatar_url || ""}
-                alt={user?.email || "User"}
-                className="w-8 h-8 rounded-full mr-2"
-              />
-              <div className="flex flex-col items-start mr-2">
-                <p className="text-xs text-gray-500">{user?.email}</p>
-              </div>
-              <FiChevronDown />
-            </div>
-          </div>
-          <div
-            id="user-menu"
-            className="absolute top-full right-0 bg-white shadow-md rounded-lg z-10 overflow-hidden transition-all"
-          >
-            <div className="p-2 flex items-center cursor-pointer bg-white">
-              <img
-                src={userBasicData?.avatar_url || ""}
-                alt={user?.email || "User"}
-                className="w-12 h-12 rounded-full mr-2"
-              />
-              <div className="flex flex-col items-start">
-                <p className="text-base text-black">
-                  {user?.user_metadata.first_name || ""} {user?.user_metadata.last_name || ""}
-                </p>
-              </div>
-            </div>
-
-            <hr className="my-2" />
-
-            <NextLink
-              href={`/profile/${user?.id}`}
-              className="px-4 py-2 flex items-center gap-2 cursor-pointer text-black"
-            >
-              <FiUser />
-              Profile
-            </NextLink>
-
-            <div
-              onClick={logout}
-              className="px-4 py-2 flex items-center gap-2 cursor-pointer text-black"
-            >
-              <FiLogOut />
-              Sign out
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Dropdown>
+      <DropdownTrigger>
+        <Button variant="bordered">
+          <Avatar
+            src={userBasicData?.avatar_url || ""}
+            name={userBasicData?.username || ""}
+            className="w-6 h-6 text-tiny"
+            showFallback
+          />
+          {user?.email}
+          <FiChevronDown />
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu aria-label="Dynamic Actions">
+        <DropdownItem key="profile" startContent={<FiUser />} href={`/profile/${user?.id}`}>
+          Profile
+        </DropdownItem>
+        <DropdownItem key="sigOut" startContent={<FiLogOut />} onClick={logout}>
+          Sign out
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
   );
 };
