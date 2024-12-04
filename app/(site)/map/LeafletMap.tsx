5a95b909e53d807b4491023db05ee02bc2d02cf7
znareak <libardojesusrengifo129@gmail.com>
2024-12-04 15:21:39 +0100

    fix import hook

--- app/(site)/map/LeafletMap.tsx
+++ app/(site)/map/LeafletMap.tsx
@@ -7,7 +7,7 @@ import { useGeolocation } from "@uidotdev/usehooks";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useEffect, useState } from "react";
 import MapLoader from "@/components/MapLoader";
-import useUbication from "../../hooks/useUbication";
+import useUbication from "../../../hooks/useUbication";
 import RecenterAutomatically from "./RecenterAutomatically";
 import { HEADER_HEIGHT } from "@/const/stylesConst";
 
